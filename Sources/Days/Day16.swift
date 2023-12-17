d1db1e5a3f10542bbb7ac9b8e0ae3287ba14ce44
Matt Pennig <mpennig@slack-corp.com>
2023-12-16 20:42:26 -0600

    Day 16 redux
    Optimizations!

--- Sources/Days/Day16.swift
+++ Sources/Days/Day16.swift
@@ -1,109 +1,97 @@
+import Collections
 import Foundation
 
 struct Day16: AdventDay {
     var data: Data
 
+    func field() -> Field {
+        Field(mirrors: data.substrings().map { $0.utf8.map(Mirror.init(rawValue:)) })
+    }
+
     func part1() throws -> Any {
-        var locations = locations()
-        walk(&locations)
-        return locations.reduce(0) {
-            $0 + $1.map({ $0.visits.isEmpty ? 0 : 1 }).reduce(0, +)
-        }
+        field().activationCount(from: .init(coordinate: .init(0, 0), moving: .right))
     }
 
     func part2() throws -> Any {
-        var maxScore = 0
-        let original = locations()
-        for y in original.indices {
-            var locations = original
-            walk(&locations, start: (0, y), moving: .right)
-            let score = locations.reduce(0) {
-                $0 + $1.map({ $0.visits.isEmpty ? 0 : 1 }).reduce(0, +)
-            }
-            maxScore = max(maxScore, score)
+        let field = self.field()
+        let width = field.mirrors[0].indices.last!
+        let height = field.mirrors.indices.last!
+        var locations = [Location]()
+
+        for y in (0...height) {
+            locations.append(.init(coordinate: .init(0, y), moving: .right))
+            locations.append(.init(coordinate: .init(width, y), moving: .left))
         }
-        for y in original.indices {
-            var locations = original
-            walk(&locations, start: (original[0].count-1, y), moving: .left)
-            let score = locations.reduce(0) {
-                $0 + $1.map({ $0.visits.isEmpty ? 0 : 1 }).reduce(0, +)
-            }
-            maxScore = max(maxScore, score)
-        }
-        for x in original[0].indices {
-            var locations = original
-            walk(&locations, start: (x, 0), moving: .down)
-            let score = locations.reduce(0) {
-                $0 + $1.map({ $0.visits.isEmpty ? 0 : 1 }).reduce(0, +)
-            }
-            maxScore = max(maxScore, score)
-        }
-        for x in original.indices {
-            var locations = original
-            walk(&locations, start: (x, original.count-1), moving: .up)
-            let score = locations.reduce(0) {
-                $0 + $1.map({ $0.visits.isEmpty ? 0 : 1 }).reduce(0, +)
-            }
-            maxScore = max(maxScore, score)
+        for x in (0...width) {
+            locations.append(.init(coordinate: .init(x, 0), moving: .down))
+            locations.append(.init(coordinate: .init(x, height), moving: .up))
         }
 
-        return maxScore
-    }
-
-    func walk(_ grid: inout [[Location]], start: (Int, Int) = (0, 0), moving: Direction = .right) {
-        let vIndices = grid.indices
-        let hIndices = grid[0].indices
-
-        var current = start
-        var moving = moving
-        while hIndices.contains(current.0) && vIndices.contains(current.1) {
-            if grid[current.1][current.0].visits.contains(moving) {
-                // We've been here before.
-                // Unlike light, we cannot span the infinite.
-                return
-            }
-
-            grid[current.1][current.0].visits.insert(moving)
-            let to = grid[current.1][current.0].mirror.direction(moving: moving)
-            for d in to.dropFirst() {
-                let next = d.next
-                let start = (current.0 + next.0, current.1 + next.1)
-                walk(&grid, start: start, moving: d)
-            }
-            moving = to[0]
-            let next = moving.next
-            current = (current.0 + next.0, current.1 + next.1)
+        return locations.reduce(0) {
+            max($0, field.activationCount(from: $1))
         }
     }
 
-    func locations() -> [[Location]] {
-        data.substrings().map {
-            $0.utf8.map {
-                Location(mirror: Mirror.init(rawValue: $0))
+    class Field {
+        let mirrors: [[Mirror]]
+        private(set) lazy var hIndices = mirrors[0].indices
+        private(set) lazy var vIndices = mirrors.indices
+
+        init(mirrors: [[Mirror]]) {
+            self.mirrors = mirrors
+        }
+
+        func activationCount(from: Location) -> Int {
+            return activatedCoordinates(from: from).filter { $0 > 0 }.count
+        }
+
+        private func activatedCoordinates(from: Location) -> [UInt8] {
+            var input = Deque<Location>()
+            var output = [UInt8](repeating: 0, count: hIndices.count * vIndices.count)
+            input.append(from)
+
+            while let current = input.popFirst() {
+                guard hIndices.contains(current.coordinate.x) && vIndices.contains(current.coordinate.y) else {
+                    continue
+                }
+
+                let index = current.coordinate.x*(vIndices.count)+current.coordinate.y
+                if output[index] & current.moving.rawValue != 0 {
+                    // We've been here before.
+                    // But, unlike light, we cannot span the infinite.
+                    continue
+                }
+                output[index] |= current.moving.rawValue
+
+                let mirror = mirrors[current.coordinate.y][current.coordinate.x]
+                let newDirections = mirror.direction(moving: current.moving)
+                for d in newDirections {
+                    input.append(.init(coordinate: d.next(current.coordinate), moving: d))
+                }
             }
+            return output
         }
     }
 
-    struct Start {
-        let x: Int
-        let y: Int
+    struct Location: Hashable {
+        let coordinate: Coordinate
         let moving: Direction
     }
 
-    struct Location {
-        let mirror: Mirror
-        var visits = Set<Direction>()
+    struct Coordinate: Hashable {
+        let x, y: Int
+        init(_ x: Int, _ y: Int) { self.x = x; self.y = y }
     }
 
-    enum Direction {
-        case up, down, left, right
+    enum Direction: UInt8, Hashable {
+        case up = 1, down = 2, left = 4, right = 8
 
-        var next: (Int, Int) {
+        func next(_ coordinate: Coordinate) -> Coordinate {
             switch self {
-            case .up: (0, -1)
-            case .down: (0, 1)
-            case .left: (-1, 0)
-            case .right: (1, 0)
+            case .up: .init(coordinate.x, coordinate.y - 1)
+            case .down: .init(coordinate.x, coordinate.y + 1)
+            case .left: .init(coordinate.x - 1, coordinate.y)
+            case .right: .init(coordinate.x + 1, coordinate.y)
             }
         }
     }
@@ -120,34 +108,48 @@ struct Day16: AdventDay {
             default: .none
             }
         }
+        
+        private static let leftDestination = [Direction.left]
+        private static let rightDestination = [Direction.right]
+        private static let upDestination = [Direction.up]
+        private static let downDestination = [Direction.down]
+        private static let upDownDestinations = [Direction.up, Direction.down]
+        private static let leftRightDestinations = [Direction.left, Direction.right]
 
         func direction(moving prev: Direction) -> [Direction] {
             switch self {
             case .none:
-                 return [prev]
+                return switch prev {
+                case .up: Self.upDestination
+                case .down: Self.downDestination
+                case .left: Self.leftDestination
+                case .right: Self.rightDestination
+                }
             case .left:
                 return switch prev {
-                case .up: [.left]
-                case .down: [.right]
-                case .left: [.up]
-                case .right: [.down]
+                case .up: Self.leftDestination
+                case .down: Self.rightDestination
+                case .left: Self.upDestination
+                case .right: Self.downDestination
                 }
             case .right:
                 return switch prev {
-                case .up: [.right]
-                case .down: [.left]
-                case .left: [.down]
-                case .right: [.up]
+                case .up: Self.rightDestination
+                case .down: Self.leftDestination
+                case .left: Self.downDestination
+                case .right: Self.upDestination
                 }
             case .vertical:
                 return switch prev {
-                case .up, .down: [prev]
-                case .left, .right: [.up, .down]
+                case .up: Self.upDestination
+                case .down: Self.downDestination
+                case .left, .right: Self.upDownDestinations
                 }
             case .horizontal:
                 return switch prev {
-                case .up, .down: [.left, .right]
-                case .left, .right: [prev]
+                case .up, .down: Self.leftRightDestinations
+                case .left: Self.leftDestination
+                case .right: Self.rightDestination
                 }
             }
         }
